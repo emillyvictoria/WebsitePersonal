3625ed55d9ea4b435d2205776d6277d77e593f52
emillyvictoria <emillyvsp02@gmail.com>
2023-06-20 11:52:28 -0300

    websitePersonal

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center:true,
+        navText:[
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive:{
+            0:{
+                items:1,
+
+            },
+            600:{
+                items:3,
+
+            },
+            1000:{
+                items:3,
+
+            }
+        }
+    })
+});
+
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => { 
+    header.classList.toggle('sticky', window.scrollY > 0);  
+ });
